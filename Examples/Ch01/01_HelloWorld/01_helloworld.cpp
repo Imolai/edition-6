1daf6472aae1d2a6a5bf796a0df9feacaab9fb27
Imolai <38439709+Imolai@users.noreply.github.com>
2024-10-17 17:44:14 +0200

    Fix 01_helloworld.cpp and add CMakeLists.txt

--- Examples/Ch01/01_HelloWorld/01_helloworld.cpp
+++ Examples/Ch01/01_HelloWorld/01_helloworld.cpp
@@ -1,9 +1,15 @@
 // 01_helloworld.cpp
 
-import std;
+// Clang++ and g++ does not support using modules on:
+//     - Ubuntu 24.04.1 LTS: clang++ v18.1.3; g++ v13.2.0 and
+//     - Termux v0.118.1: clang++ v19.1.2.
+// Therefore `import std` and `std::println()` have been replaced.
 
-int main()
-{
-	std::println("Hello, World!");
+//import std;
+#include <iostream>
+
+int main() {
+	//std::println("Hello, World!");
+	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
